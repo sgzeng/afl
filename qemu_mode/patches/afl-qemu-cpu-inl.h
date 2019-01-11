8e4e2b1613f36f478a2f14577792f554a7af8a5d
haochen <haochen.zeng@cernet.edu.cn>
2019-01-10 20:53:05 -0800

    pull from latest version

--- qemu_mode/patches/afl-qemu-cpu-inl.h
+++ qemu_mode/patches/afl-qemu-cpu-inl.h
@@ -7,9 +7,6 @@
 
    Idea & design very much by Andrew Griffiths.
 
-   TCG instrumentation and block chaining support by Andrea Biondo
-                                      <andrea.biondo965@gmail.com>
-
    Copyright 2015, 2016, 2017 Google Inc. All rights reserved.
 
    Licensed under the Apache License, Version 2.0 (the "License");
@@ -36,6 +33,15 @@
  * VARIOUS AUXILIARY STUFF *
  ***************************/
 
+/* A snippet patched into tb_find_slow to inform the parent process that
+   we have hit a new block that hasn't been translated yet, and to tell
+   it to translate within its own context, too (this avoids translation
+   overhead in the next forked-off copy). */
+
+#define AFL_QEMU_CPU_SNIPPET1 do { \
+    afl_request_tsl(pc, cs_base, flags); \
+  } while (0)
+
 /* This snippet kicks in when the instruction pointer is positioned at
    _start and does the usual forkserver stuff, not very different from
    regular instrumentation injected via afl-as.h. */
@@ -45,6 +51,7 @@
       afl_setup(); \
       afl_forkserver(cpu); \
     } \
+    afl_maybe_log(itb->pc); \
   } while (0)
 
 /* We use one additional file descriptor to relay "needs translation"
@@ -54,7 +61,7 @@
 
 /* This is equivalent to afl-as.h: */
 
-unsigned char *afl_area_ptr; /* Exported for afl_gen_trace */
+static unsigned char *afl_area_ptr;
 
 /* Exported variables populated by the code patched into elfload.c: */
 
@@ -69,32 +76,23 @@ unsigned int afl_forksrv_pid;
 
 /* Instrumentation ratio: */
 
-unsigned int afl_inst_rms = MAP_SIZE; /* Exported for afl_gen_trace */
+static unsigned int afl_inst_rms = MAP_SIZE;
 
 /* Function declarations. */
 
 static void afl_setup(void);
 static void afl_forkserver(CPUState*);
+static inline void afl_maybe_log(abi_ulong);
 
 static void afl_wait_tsl(CPUState*, int);
-static void afl_request_tsl(target_ulong, target_ulong, uint32_t, TranslationBlock*, int);
+static void afl_request_tsl(target_ulong, target_ulong, uint64_t);
 
-/* Data structures passed around by the translate handlers: */
-
-struct afl_tb {
-  target_ulong pc;
-  target_ulong cs_base;
-  uint32_t flags;
-};
+/* Data structure passed around by the translate handlers: */
 
 struct afl_tsl {
-  struct afl_tb tb;
-  char is_chain;
-};
-
-struct afl_chain {
-  struct afl_tb last_tb;
-  int tb_exit;
+  target_ulong pc;
+  target_ulong cs_base;
+  uint64_t flags;
 };
 
 /* Some forward decls: */
@@ -226,36 +224,53 @@ static void afl_forkserver(CPUState *cpu) {
 }
 
 
-/* This code is invoked whenever QEMU decides that it doesn't have a
-   translation of a particular block and needs to compute it, or when it
-   decides to chain two TBs together. When this happens, we tell the parent to
-   mirror the operation, so that the next fork() has a cached copy. */
+/* The equivalent of the tuple logging routine from afl-as.h. */
 
-static void afl_request_tsl(target_ulong pc, target_ulong cb, uint32_t flags,
-                            TranslationBlock *last_tb, int tb_exit) {
+static inline void afl_maybe_log(abi_ulong cur_loc) {
+
+  static __thread abi_ulong prev_loc;
+
+  /* Optimize for cur_loc > afl_end_code, which is the most likely case on
+     Linux systems. */
+
+  if (cur_loc > afl_end_code || cur_loc < afl_start_code || !afl_area_ptr)
+    return;
+
+  /* Looks like QEMU always maps to fixed locations, so ASAN is not a
+     concern. Phew. But instruction addresses may be aligned. Let's mangle
+     the value to get something quasi-uniform. */
+
+  cur_loc  = (cur_loc >> 4) ^ (cur_loc << 8);
+  cur_loc &= MAP_SIZE - 1;
+
+  /* Implement probabilistic instrumentation by looking at scrambled block
+     address. This keeps the instrumented locations stable across runs. */
+
+  if (cur_loc >= afl_inst_rms) return;
+
+  afl_area_ptr[cur_loc ^ prev_loc]++;
+  prev_loc = cur_loc >> 1;
+
+}
+
+
+/* This code is invoked whenever QEMU decides that it doesn't have a
+   translation of a particular block and needs to compute it. When this happens,
+   we tell the parent to mirror the operation, so that the next fork() has a
+   cached copy. */
+
+static void afl_request_tsl(target_ulong pc, target_ulong cb, uint64_t flags) {
 
   struct afl_tsl t;
-  struct afl_chain c;
 
   if (!afl_fork_child) return;
 
-  t.tb.pc      = pc;
-  t.tb.cs_base = cb;
-  t.tb.flags   = flags;
-  t.is_chain   = (last_tb != NULL);
+  t.pc      = pc;
+  t.cs_base = cb;
+  t.flags   = flags;
 
   if (write(TSL_FD, &t, sizeof(struct afl_tsl)) != sizeof(struct afl_tsl))
     return;
-
-  if (t.is_chain) {
-    c.last_tb.pc      = last_tb->pc;
-    c.last_tb.cs_base = last_tb->cs_base;
-    c.last_tb.flags   = last_tb->flags;
-    c.tb_exit         = tb_exit;
-
-    if (write(TSL_FD, &c, sizeof(struct afl_chain)) != sizeof(struct afl_chain))
-      return;
-  }
 
 }
 
@@ -265,8 +280,7 @@ static void afl_request_tsl(target_ulong pc, target_ulong cb, uint32_t flags,
 static void afl_wait_tsl(CPUState *cpu, int fd) {
 
   struct afl_tsl t;
-  struct afl_chain c;
-  TranslationBlock *tb, *last_tb;
+  TranslationBlock *tb;
 
   while (1) {
 
@@ -275,29 +289,14 @@ static void afl_wait_tsl(CPUState *cpu, int fd) {
     if (read(fd, &t, sizeof(struct afl_tsl)) != sizeof(struct afl_tsl))
       break;
 
-    tb = tb_htable_lookup(cpu, t.tb.pc, t.tb.cs_base, t.tb.flags);
+    tb = tb_htable_lookup(cpu, t.pc, t.cs_base, t.flags);
 
     if(!tb) {
       mmap_lock();
       tb_lock();
-      tb = tb_gen_code(cpu, t.tb.pc, t.tb.cs_base, t.tb.flags, 0);
+      tb_gen_code(cpu, t.pc, t.cs_base, t.flags, 0);
       mmap_unlock();
       tb_unlock();
-    }
-
-    if (t.is_chain) {
-      if (read(fd, &c, sizeof(struct afl_chain)) != sizeof(struct afl_chain))
-        break;
-
-      last_tb = tb_htable_lookup(cpu, c.last_tb.pc, c.last_tb.cs_base,
-                                 c.last_tb.flags);
-      if (last_tb) {
-        tb_lock();
-        if (!tb->invalid) {
-          tb_add_jump(last_tb, c.tb_exit, tb);
-        }
-        tb_unlock();
-      }
     }
 
   }
